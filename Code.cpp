3b505010eebc415d2149bfbd9234464f6ae71c2d
Vaino <tatrongnghiak15@siu.edu.vn>
2024-09-18 11:17:32 +0700

    Push cpp

---
+++ Code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    // Nhập số a
+    cout << "Nhập số a: ";
+    cin >> a;
+
+    // Nhập số b
+    cout << "Nhập số b: ";
+    cin >> b;
+
+    // Hiển thị kết quả
+    cout << "Số a là: " << a << endl;
+    cout << "Số b là: " << b << endl;
+
+    return 0;
+}
